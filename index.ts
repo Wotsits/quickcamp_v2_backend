24bf2397307a5bd18fceac0042449550a6817deb
Simon Sexton <simon.sexton@live.co.uk>
2023-09-09 00:15:07 +0100

    feat: add middleware for auth

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-import express, { Request, Response } from 'express';
-import dotenv from 'dotenv';
-import { middlewareInit } from './middlewareInit.js';
-import { routesInit } from './routes.js';
-import { PrismaClient } from '@prisma/client';
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import { middlewareInit } from "./middlewareInit.js";
+import { routesInit } from "./routes.js";
+import { PrismaClient } from "@prisma/client";
 
 // -----------------
 // APP SETUP
@@ -11,33 +11,39 @@ import { PrismaClient } from '@prisma/client';
 dotenv.config();
 const app = express();
 
-// Instantiate Prisma instance
-const prisma = new PrismaClient({ log: ['query', 'info', 'warn', 'error'] })
+// Instantiate Prisma instance with logging
+const prisma = new PrismaClient({ log: ["query", "info", "warn", "error"] });
 
 // -----------------
 // ENV VARS
 // -----------------
 
-const {PORT: port, JWTSECRET: jwtSecret} = process.env;
+const { PORT: port, JWTSECRET: jwtSecret } = process.env;
 
 // -----------------
 // MIDDLEWARE SETUP
 // -----------------
 
-if (!jwtSecret) throw new Error("JWTSecret undefined in env.  Define listening port as JWTSECRET={jwtsecret} in .env")
 middlewareInit(app, express);
 
 // ----------------
 // ROUTES
 // ----------------
 
-routesInit(app, prisma, jwtSecret)
+if (!jwtSecret)
+  throw new Error(
+    "JWTSecret undefined in env.  Define listening port as JWTSECRET={jwtsecret} in .env"
+  );
+routesInit(app, prisma, jwtSecret);
 
 // ----------------
 // LISTEN
 // ----------------
 
-if (!port) throw new Error("Listening port undefined by env.  Define listening port as PORT={portNumber} in .env")
+if (!port)
+  throw new Error(
+    "Listening port undefined by env.  Define listening port as PORT={portNumber} in .env"
+  );
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
---
+++ utilities/userManagement/middleware.ts
@@ -0,0 +1,55 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+const { JWTSECRET: jwtSecret } = process.env;
+
+export function adminAuth(req: Request, res: Response, next: NextFunction) {
+  if (!jwtSecret)
+    throw new Error(
+      "JWTSecret undefined in env.  Define listening port as JWTSECRET={jwtsecret} in .env"
+    );
+  const token = req.cookies.jwt;
+  if (token) {
+    jwt.verify(token, jwtSecret, (err: any, decodedToken: any) => {
+      if (err) {
+        return res.status(401).json({ message: "Not authorized" });
+      } else {
+        if (decodedToken.role !== "ADMIN") {
+          return res.status(401).json({ message: "Not authorized" });
+        } else {
+          next();
+        }
+      }
+    });
+  } else {
+    return res
+      .status(401)
+      .json({ message: "Not authorized, token not available" });
+  }
+}
+
+
+export function userAuth(req: Request, res: Response, next: NextFunction) {
+    if (!jwtSecret)
+    throw new Error(
+      "JWTSecret undefined in env.  Define listening port as JWTSECRET={jwtsecret} in .env"
+    );
+    const token = req.cookies.jwt
+    if (token) {
+      jwt.verify(token, jwtSecret, (err: any, decodedToken: any) => {
+        if (err) {
+          return res.status(401).json({ message: "Not authorized" })
+        } else {
+          if (decodedToken.role !== "BASIC") {
+            return res.status(401).json({ message: "Not authorized" })
+          } else {
+            next()
+          }
+        }
+      })
+    } else {
+      return res
+        .status(401)
+        .json({ message: "Not authorized, token not available" })
+    }
+  }
