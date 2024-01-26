369b496ab1bd1e4dabbeb14a0a4bee9dfdd1b831
Simon Sexton <simon.sexton@live.co.uk>
2024-01-26 18:29:38 +0000

    refactor: use router-controller seperation for users routes

---
+++ modules/users/usersControllers.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import { prisma } from "../..";
+
+export async function getUsers(req: Request, res: Response) {
+  const { user } = req;
+  if (!user) {
+    res.status(401).send({ message: "Unauthorized" });
+    return;
+  }
+
+  const { tenantId } = user;
+  if (!tenantId) {
+    res
+      .status(401)
+      .send({
+        message:
+          "Tenant id not accessible on user object.  This is a backend issue.",
+      });
+    return;
+  }
+
+  const users = await prisma.user.findMany({
+    where: { tenantId },
+    include: {
+      roles: true,
+    },
+  });
+
+  return res.status(200).json({ data: users });
+}
--- modules/users/usersRoutes.ts
+++ modules/users/usersRoutes.ts
@@ -1,30 +1,9 @@
-import { Request, Response } from "express";
 import { urls } from "../../enums.js";
 import { loggedIn } from "../../utilities/middleware/userManagement/middleware.js";
 import { validateProvidedData } from "../../utilities/middleware/validation/middleware.js";
-import { app, prisma } from "../../index.js";
+import { app } from "../../index.js";
+import { getUsers } from "./usersControllers.js";
 
 export function registerUserRoutes() {
-  app.get(urls.USERS, validateProvidedData, loggedIn, async (req: Request, res: Response) => {
-    const { user } = req;
-    if (!user) {
-      res.status(401).send({ message: "Unauthorized" });
-      return;
-    }
-
-    const { tenantId } = user;
-    if (!tenantId) {
-      res.status(401).send({ message: "Tenant id not accessible on user object.  This is a backend issue." });
-      return;
-    }
-
-    const users = await prisma.user.findMany({
-      where: { tenantId },
-      include: {
-        roles: true,
-      },
-    });
-
-    return res.status(200).json({ data: users });
-  });
+  app.get(urls.USERS, validateProvidedData, loggedIn, getUsers);
 }
