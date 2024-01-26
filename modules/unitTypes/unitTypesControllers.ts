3da2de7699e6b9c8da183ce25e518f579746b9fd
Simon Sexton <simon.sexton@live.co.uk>
2024-01-26 18:27:59 +0000

    refactor: use router-controller seperation for unit types routes

---
+++ modules/unitTypes/unitTypesControllers.ts
@@ -0,0 +1,117 @@
+import { Request, Response } from "express";
+import { prisma } from "../..";
+
+export async function getUnitTypes(req: Request, res: Response) {
+  // check if user is logged in
+  const { user } = req;
+  if (!user) {
+    res.status(401).json({ message: "Unauthorized." });
+    return;
+  }
+
+  const {
+    siteId,
+    includeSite,
+    includeUnits,
+    includeRates,
+    startDate,
+    endDate,
+  } = req.query;
+
+  const { tenantId } = user;
+
+  if (!tenantId) {
+    res.status(401).json({
+      message:
+        "Tenant id not accessible on user object.  This is a backend issue.",
+    });
+    return;
+  }
+
+  const parsedincludeSite = includeSite === "true";
+  const parsedincludeUnits = includeUnits === "true";
+  const parsedincludeRates = includeRates === "true";
+
+  // if siteId is not provided, return all unit-types for the tenant.
+  if (!siteId) {
+    const data = await prisma.unitType.findMany({
+      where: {
+        site: {
+          tenantId: tenantId,
+        },
+      },
+      include: {
+        units: parsedincludeUnits,
+      },
+    });
+    return res.status(200).json({ data });
+  }
+
+  // if siteId is provided, return all unit-types for the site.
+  else {
+    if (parsedincludeRates && (!startDate || !endDate)) {
+      return res.status(400).json({
+        message:
+          "If includeRates is true, startDate and endDate must be provided.",
+      });
+    }
+
+    const data = await prisma.unitType.findMany({
+      where: {
+        siteId: parseInt(siteId as string),
+      },
+      include: {
+        units: parsedincludeUnits,
+        unitTypeFeesCalendarEntries: parsedincludeRates && {
+          where: {
+            date: {
+              lte: new Date(endDate as string),
+              gte: new Date(startDate as string),
+            },
+          },
+        },
+        guestFeesCalendarEntries: parsedincludeRates && {
+          where: {
+            date: {
+              lte: new Date(endDate as string),
+              gte: new Date(startDate as string),
+            },
+          },
+          include: {
+            guestType: true,
+          },
+        },
+        petFeesCalendarEntries: parsedincludeRates && {
+          where: {
+            date: {
+              lte: new Date(endDate as string),
+              gte: new Date(startDate as string),
+            },
+          },
+        },
+        vehicleFeesCalendarEntries: parsedincludeRates && {
+          where: {
+            date: {
+              lte: new Date(endDate as string),
+              gte: new Date(startDate as string),
+            },
+          },
+        },
+        extraFeesCalendarEntries: parsedincludeRates && {
+          where: {
+            date: {
+              lte: new Date(endDate as string),
+              gte: new Date(startDate as string),
+            },
+          },
+        },
+      },
+    });
+
+    return res.status(200).json({ data });
+  }
+}
+
+export async function getUnitTypeById(req: Request, res: Response) {
+  return res.status(501).json({ message: "Not implemented" });
+}
--- modules/unitTypes/unitTypesRoutes.ts
+++ modules/unitTypes/unitTypesRoutes.ts
@@ -1,11 +1,11 @@
-import { Request, Response } from "express";
 import { urls } from "../../enums.js";
 import {
   hasAccessToRequestedSite,
   loggedIn,
 } from "../../utilities/middleware/userManagement/middleware.js";
 import { validateProvidedData } from "../../utilities/middleware/validation/middleware.js";
-import { app, prisma } from "../../index.js";
+import { app } from "../../index.js";
+import { getUnitTypeById, getUnitTypes } from "./unitTypesControllers.js";
 
 export function registerUnitTypeRoutes() {
   app.get(
@@ -13,128 +13,8 @@ export function registerUnitTypeRoutes() {
     validateProvidedData,
     loggedIn,
     hasAccessToRequestedSite,
-    async (req: Request, res: Response) => {
-      // check if user is logged in
-      const { user } = req;
-      if (!user) {
-        res.status(401).json({ message: "Unauthorized." });
-        return;
-      }
-
-      const {
-        siteId,
-        includeSite,
-        includeUnits,
-        includeRates,
-        startDate,
-        endDate,
-      } = req.query;
-
-      const { tenantId } = user;
-
-      if (!tenantId) {
-        res
-          .status(401)
-          .json({
-            message:
-              "Tenant id not accessible on user object.  This is a backend issue.",
-          });
-        return;
-      }
-
-      const parsedincludeSite = includeSite === "true";
-      const parsedincludeUnits = includeUnits === "true";
-      const parsedincludeRates = includeRates === "true";
-
-      // if siteId is not provided, return all unit-types for the tenant.
-      if (!siteId) {
-        const data = await prisma.unitType.findMany({
-          where: {
-            site: {
-              tenantId: tenantId,
-            },
-          },
-          include: {
-            units: parsedincludeUnits,
-          },
-        });
-        return res.status(200).json({ data });
-      }
-
-      // if siteId is provided, return all unit-types for the site.
-      else {
-
-        if (parsedincludeRates && (!startDate || !endDate)) {
-          return res
-            .status(400)
-            .json({
-              message:
-                "If includeRates is true, startDate and endDate must be provided.",
-            });
-        }
-
-        const data = await prisma.unitType.findMany({
-          where: {
-            siteId: parseInt(siteId as string),
-          },
-          include: {
-            units: parsedincludeUnits,
-            unitTypeFeesCalendarEntries: parsedincludeRates && {
-              where: {
-                date: {
-                  lte: new Date(endDate as string),
-                  gte: new Date(startDate as string),
-                },
-              },
-            },
-            guestFeesCalendarEntries: parsedincludeRates && {
-              where: {
-                date: {
-                  lte: new Date(endDate as string),
-                  gte: new Date(startDate as string),
-                },
-              },
-              include: {
-                guestType: true,
-              },
-            },
-            petFeesCalendarEntries: parsedincludeRates && {
-              where: {
-                date: {
-                  lte: new Date(endDate as string),
-                  gte: new Date(startDate as string),
-                },
-              },
-            },
-            vehicleFeesCalendarEntries: parsedincludeRates && {
-              where: {
-                date: {
-                  lte: new Date(endDate as string),
-                  gte: new Date(startDate as string),
-                },
-              },
-            },
-            extraFeesCalendarEntries: parsedincludeRates && {
-              where: {
-                date: {
-                  lte: new Date(endDate as string),
-                  gte: new Date(startDate as string),
-                },
-              },
-            },
-          },
-        });
-
-        return res.status(200).json({ data });
-      }
-    }
+    getUnitTypes
   );
 
-  app.get(
-    `${urls.UNITTYPES}/:id`,
-    loggedIn,
-    async (req: Request, res: Response) => {
-      return res.status(501).json({ message: "Not implemented" });
-    }
-  );
+  app.get(`${urls.UNITTYPES}/:id`, loggedIn, getUnitTypeById);
 }
