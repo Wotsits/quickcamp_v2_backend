67628f3785833469dc5e037edd6edbc55c6ec591
Simon Sexton <simon.sexton@live.co.uk>
2024-01-26 18:22:27 +0000

    refactor: use router-controller seperation for stats routes

---
+++ modules/stats/statsControllers.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from "express";
+import { prisma } from "../..";
+
+export async function getTotalOnSite(req: Request, res: Response) {
+  const { user } = req;
+  if (!user) {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+
+  const { siteId } = req.query;
+
+  if (!siteId) {
+    return res.status(400).json({
+      message:
+        "Bad request - invalid parameters.  You must provide a siteId to which you have access.",
+    });
+  }
+
+  // parse the siteId
+  const parsedSiteId = parseInt(siteId as string);
+
+  // get the total number of guests on site
+  const totalOnSite = await prisma.bookingGuest.count({
+    where: {
+      booking: {
+        unit: {
+          unitType: {
+            siteId: parsedSiteId,
+          },
+        },
+      },
+      NOT: {
+        checkedIn: null,
+      },
+      checkedOut: null,
+    },
+  });
+
+  return res.status(200).json({ data: { totalOnSite } });
+}
--- modules/stats/statsRoutes.ts
+++ modules/stats/statsRoutes.ts
@@ -6,53 +6,14 @@ import {
 } from "../../utilities/middleware/userManagement/middleware.js";
 import { validateProvidedData } from "../../utilities/middleware/validation/middleware.js";
 import { app, prisma } from "../../index.js";
+import { getTotalOnSite } from "./statsControllers.js";
 
 export function registerStatsRoutes() {
-  // ****************************************************
-
   app.get(
     urls.STATS + urls.ON_SITE,
     validateProvidedData,
     loggedIn,
     hasAccessToRequestedSite,
-    async (req: Request, res: Response) => {
-      const { user } = req;
-      if (!user) {
-        return res.status(401).json({
-          message: "Unauthorized",
-        });
-      }
-
-      const { siteId } = req.query;
-
-      if (!siteId) {
-        return res.status(400).json({
-          message:
-            "Bad request - invalid parameters.  You must provide a siteId to which you have access.",
-        });
-      }
-
-      // parse the siteId
-      const parsedSiteId = parseInt(siteId as string);
-
-      // get the total number of guests on site
-      const totalOnSite = await prisma.bookingGuest.count({
-        where: {
-          booking: {
-            unit: {
-              unitType: {
-                siteId: parsedSiteId,
-              },
-            },
-          },
-          NOT: {
-            checkedIn: null,
-          },
-          checkedOut: null,
-        },
-      });
-
-      return res.status(200).json({ data: { totalOnSite } });
-    }
+    getTotalOnSite
   );
 }
