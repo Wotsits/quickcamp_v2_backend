3ab7fcbd7258399fc187a29824853be05c47ab6a
Simon Sexton <simon.sexton@live.co.uk>
2024-01-26 12:55:26 +0000

    refactor: use router-controller seperation for guest types routes.

---
+++ modules/guestTypes/guestTypesControllers.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from "express";
+import { prisma } from "../../index.js";
+
+export async function getGuestTypes (req: Request, res: Response) {
+    // belt and braces check that the user is logged in and has a tenantId
+    const { user } = req;
+    if (!user) {
+      res.status(401).json({ message: "Not logged in." });
+      return;
+    }
+
+    const { tenantId } = user;
+
+    if (!tenantId) {
+      res.status(401).json({ message: "Not logged in." });
+      return;
+    }
+
+    const { siteId } = req.query;
+
+    // if siteId is not provided, return all guest-types for the tenant.
+    if (!siteId) {
+      const data = await prisma.guestType.findMany({
+        where: {
+          site: {
+            tenantId: tenantId,
+          },
+        },
+      });
+      return res.status(200).json({ data });
+    }
+    // if siteId is provided, return all guest-types for the site.
+    else {
+      const data = await prisma.guestType.findMany({
+        where: {
+          siteId: parseInt(siteId as string),
+        },
+      });
+      return res.status(200).json({ data });
+    }
+  }
--- modules/guestTypes/guestTypesRoutes.ts
+++ modules/guestTypes/guestTypesRoutes.ts
@@ -1,11 +1,11 @@
-import { Request, Response } from "express";
 import { urls } from "../../enums.js";
 import {
   hasAccessToRequestedSite,
   loggedIn,
 } from "../../utilities/middleware/userManagement/middleware.js";
 import { validateProvidedData } from "../../utilities/middleware/validation/middleware.js";
-import { app, prisma } from "../../index.js";
+import { app } from "../../index.js";
+import { getGuestTypes } from "./guestTypesControllers.js";
 
 export function registerGuestTypeRoutes() {
   app.get(
@@ -13,43 +13,6 @@ export function registerGuestTypeRoutes() {
     validateProvidedData,
     loggedIn,
     hasAccessToRequestedSite,
-    async (req: Request, res: Response) => {
-      // belt and braces check that the user is logged in and has a tenantId
-      const { user } = req;
-      if (!user) {
-        res.status(401).json({ message: "Not logged in." });
-        return;
-      }
-
-      const { tenantId } = user;
-
-      if (!tenantId) {
-        res.status(401).json({ message: "Not logged in." });
-        return;
-      }
-
-      const { siteId } = req.query;
-
-      // if siteId is not provided, return all guest-types for the tenant.
-      if (!siteId) {
-        const data = await prisma.guestType.findMany({
-          where: {
-            site: {
-              tenantId: tenantId,
-            },
-          },
-        });
-        return res.status(200).json({ data });
-      }
-      // if siteId is provided, return all guest-types for the site.
-      else {
-        const data = await prisma.guestType.findMany({
-          where: {
-            siteId: parseInt(siteId as string),
-          },
-        });
-        return res.status(200).json({ data });
-      }
-    }
+    getGuestTypes
   );
 }
